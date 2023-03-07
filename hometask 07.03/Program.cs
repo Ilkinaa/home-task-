64b514553dbeec4c9e37fa448db1e7d77e904d94
Ilkinaa <ilkinba@code.edu.az>
2023-03-07 14:45:37 +0400

    done

---
+++ hometask 07.03/Program.cs
@@ -0,0 +1,18 @@
+﻿while (true)
+{
+    Console.Write("Please enter Username : ");
+    string username = Console.ReadLine();
+    Console.Write("Password : ");
+    string password = Console.ReadLine();
+    user user1 = new user(username, password);
+    if (user1.Password != null && user1.UserName != null)
+    {
+        Console.WriteLine("Successfull Access");
+        break;
+
+    }
+    else
+    {
+        Console.WriteLine("Password must contain upper case,min 8 car.and digit. \nUsername and Password don't contain");
+    }
+}
